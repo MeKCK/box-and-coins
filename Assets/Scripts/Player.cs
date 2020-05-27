e30bb371a69d8d79c7b8c283e81fba2933c269cd
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-27 06:36:50 +0300

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public float speed = 10;
+    public float gravity = 10;
+    public float maxVelocityChange = 10; 
+    private bool grounded;
+
+    
+    private bool dead; //a true or false call
+    public int health;    
+    private Transform PlayerTransform;
+    private Rigidbody _rigidbody;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerTransform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        _rigidbody.freezeRotation = true;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerTransform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime  , 0);
+
+        Vector3 targetVelocity = new Vector3(0,0,Input.GetAxis("Vertical"));
+        targetVelocity = PlayerTransform.TransformDirection(targetVelocity);
+        targetVelocity = targetVelocity * speed; 
+
+        Vector3 velocity = _rigidbody.velocity;
+        Vector3 velocityChange = targetVelocity - velocity;
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+        _rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+        print(_rigidbody.velocity);
+        
+        grounded = false;
+        
+        
+    }
+
+    void OnCollisionStay(){
+        grounded = true;
+
+    }
+}
