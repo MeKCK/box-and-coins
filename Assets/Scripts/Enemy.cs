b264e02a17da9e2401f14b9997a92de0afa3498f
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-27 09:43:51 +0300

    finito

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,13 +13,23 @@ public class Enemy : MonoBehaviour
     public float Speed = .5f;
     public float GoToDistance = 13;
     public float AttackDistance = 4;
+    public float AttackTimer = 2;
     public Transform Target;
     public string PlayerTag = "Player";
+    private float CurTime;
+    private Player PlayerScript;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
+        //our start
+        Target = GameObject.FindGameObjectWithTag(PlayerTag).transform;
+        CurTime = AttackTimer;
+        if (Target != null) {
+            PlayerScript = Target.GetComponent<Player>();
+        }
         while (true){
+            // this is our update
             switch(CurState){
                 case State.LOOKFOR:
                     LookFor();
@@ -41,13 +51,45 @@ public class Enemy : MonoBehaviour
     void LookFor()
     {
        print("hi we are in Lookfor"); 
+       if (Vector3.Distance(Target.position, transform.position) < GoToDistance){
+           CurState = State.GOTO;
+
+       }
     }
     void GoTo(){
         print("hi we are in GoTo");
+        transform.LookAt(Target); 
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit Buddy;
+        if(Physics.Raycast(transform.position,fwd,out Buddy)){
+            if( Buddy.transform.tag != PlayerTag) {
+                CurState = State.LOOKFOR;
+                return;
+            }
+        }
+        if (Vector3.Distance(Target.position,transform.position)> AttackDistance){
+            transform.position = Vector3.MoveTowards(transform.position,Target.position, Speed * Time.deltaTime);
+        }
+        else {
+            CurState = State.ATTACK;
+        }
 
     }
     void Attack(){
         print ("hi we are in Attack");
+        transform.LookAt(Target);
+        CurTime = CurTime - Time.deltaTime;
+
+        if (CurTime < 0) {
+            PlayerScript.health--;
+            CurTime = AttackTimer;
+        }
+
+        if (Vector3.Distance(Target.position, transform.position) > AttackDistance){
+            CurState = State.GOTO;
+            
+            }
+
     }
 
 }
