dea528674bd65ddcb354038914aa629d5559e66f
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-27 08:38:05 +0300

    enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public enum State  {
+        LOOKFOR,
+        GOTO,
+        ATTACK,
+    }
+    public State CurState;
+    public float Speed = .5f;
+    public float GoToDistance = 13;
+    public float AttackDistance = 4;
+    public Transform Target;
+    public string PlayerTag = "Player";
+
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
+        while (true){
+            switch(CurState){
+                case State.LOOKFOR:
+                    LookFor();
+                    break;
+                case State.GOTO: 
+                    GoTo();
+                     break;
+                case State.ATTACK:
+                    Attack();
+                     break;
+
+            }
+            yield return 0;
+        }
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LookFor()
     {
-        
+       print("hi we are in Lookfor"); 
     }
+    void GoTo(){
+        print("hi we are in GoTo");
+
+    }
+    void Attack(){
+        print ("hi we are in Attack");
+    }
+
 }
